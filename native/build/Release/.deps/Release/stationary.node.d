5e6c10b86fb13ff51a9614632a0d6150cd7bc927
youngju6143 <youngju6143@naver.com>
2026-09-23 11:08:25 +0900

    chore: 네이티브 빌드 산출물 추적 제외

--- native/build/Release/.deps/Release/stationary.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/stationary.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=11.0 -arch arm64 -L./Release -stdlib=libc++  -o Release/stationary.node Release/obj.target/stationary/src/stationary.o -framework Cocoa
